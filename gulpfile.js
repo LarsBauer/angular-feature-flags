bab13c7ed03e0f482da261ded4c4316a8dba2b8f
Lars Bauer <larsbauer91@outlook.com>
2018-05-09 11:24:42 +0200

    switched tp provider for usage in app config

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
-var angularOrder = require('gulp-angular-order');
 var concat = require('gulp-concat');
 var iife = require('gulp-iife');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
+SRC_FILES = ['feature-flags.module.js', 'feature-flags.provider.js', 'feature-flag.directive.js'];
+
 gulp.task('build', function () {
-    return gulp.src('src/*.js')
+    return gulp.src(SRC_FILES)
         .pipe(plumber())
-        .pipe(angularOrder())
         .pipe(concat('angular-feature-flags.js'))
         .pipe(iife())
         .pipe(gulp.dest('dist/'))
